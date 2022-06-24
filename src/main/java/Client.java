2a2ad9ae6660c62202545ac3ea24e03782d6e143
wwwjjdk <goshasavtch@yandex.ru>
2022-06-24 21:16:28 +0300

    with hashMap

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -1,35 +1,43 @@
 import java.io.*;
 import java.net.Socket;
 import java.util.Scanner;
+import java.util.logging.FileHandler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
 
 public class Client extends DoingAll{
     private static Scanner scanner = new Scanner(System.in);
+    private static Logger logger = Logger.getLogger(Client.class.getName());
 
     public static void main(String[] args) throws IOException {
+
+        logger.setLevel(Level.INFO);
+        FileHandler fileHandler = new FileHandler("logServer.log", true);
+        logger.addHandler(fileHandler);
+        SimpleFormatter simpleFormatter = new SimpleFormatter();
+        fileHandler.setFormatter(simpleFormatter);
         readReport();
+
         int port = choice();
         Socket socket = new Socket("127.0.0.1", port);
         try (
                 BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
                 PrintWriter printWriter = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()), true)
         ) {
-            System.out.print("Enter the name: ");
+            System.out.println(bufferedReader.readLine());
             printWriter.println(scanner.nextLine());
+            logger.info("Клиент залогинился");
 
             while (true) {
                 System.out.println(bufferedReader.readLine());
-                //---------------------------------------------------------------
                 String str = scanner.nextLine();
-                System.out.println("true");
                 if(str.equalsIgnoreCase("/exit")){
+                    logger.info("Клиент вышел");
                     printWriter.println(str);
                     break;
                 }
                 printWriter.println(str);
-                //---------------------------------------------------------------
-                System.out.println(bufferedReader.readLine());
-                printWriter.println(scanner.nextLine());
-                //--------------------------------------------------------------
                 System.out.println(bufferedReader.readLine());
             }
         } catch (IOException e) {
--- src/main/java/ServerOk.java
+++ src/main/java/ServerOk.java
@@ -2,6 +2,7 @@ import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Scanner;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.logging.FileHandler;
@@ -10,11 +11,13 @@ import java.util.logging.Logger;
 import java.util.logging.SimpleFormatter;
 
 public class ServerOk extends DoingAll {
+    private static ConcurrentHashMap<Integer, Socket> concurrentHashMap = new ConcurrentHashMap<>();
     private static Logger logger = Logger.getLogger(ServerOk.class.getName());
-    private static Scanner scanner = new Scanner(System.in);
-    private static final int AMOUNT = 3;
+    private static final int AMOUNT = 5;
 
     public static void main(String[] args) throws IOException {
+        ExecutorService executorService = Executors.newFixedThreadPool(AMOUNT);
+
         logger.setLevel(Level.INFO);
         FileHandler fileHandler = new FileHandler("logServer.log", true);
         logger.addHandler(fileHandler);
@@ -22,79 +25,50 @@ public class ServerOk extends DoingAll {
         fileHandler.setFormatter(simpleFormatter);
 
         readReport();
+
         int port = choice();
         logger.info("server connected on port: " + port);
 
-        ServerSocket serverSocket = new ServerSocket(port);
-        ExecutorService executorService = Executors.newFixedThreadPool(AMOUNT);
-
-        while (true) {
-            executorService.submit(new Thread(() -> {
-                while (true) {
+        try (ServerSocket serverSocket = new ServerSocket(port)) {
+            while (true) {
+                final var clientSocket = serverSocket.accept();
+                logger.info("Клиент подключился");
+                executorService.submit(new Thread(() -> {
                     try (
-                            Socket socket = serverSocket.accept();
-                            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-                            PrintWriter printWriter = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()), true)
+                            final var in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                            final var out = new PrintWriter(new OutputStreamWriter(clientSocket.getOutputStream()), true);
                     ) {
-                        String nameOfClient = bufferedReader.readLine();
-                        logger.info("Client " + nameOfClient + " connected\n" + "(id: " + Thread.currentThread().getId() + ")");
-                        while (true) {
-                            printWriter.println("find the area of a figure:1 Cylinder; 2 Cube; 3 Cone.Enter a number or /exit: ");
-                            logger.info("request sent to client " + nameOfClient);
+                        concurrentHashMap.put(clientSocket.getLocalPort(), clientSocket);
+                        logger.info("Клиент добавлен в мапу");
+                        out.println("Введите имя:");
 
-                            String str = bufferedReader.readLine();
-                            if (str.equalsIgnoreCase("/exit")) {
-                                logger.info("the client leaves the server");
+                        String name = in.readLine();
+                        logger.info("Пользователь: " + name + "\nПорт:" + clientSocket.getPort());
+                        while (true) {
+                            out.println("Введите сообщение");
+                            String exitOrNo = in.readLine();
+                            if (exitOrNo.equalsIgnoreCase("/exit")) {
+                                logger.info("Пользователь вышел");
+                                concurrentHashMap.remove(clientSocket.getLocalPort());
                                 break;
                             }
-                            switch (str) {
-                                case "1":
-                                    printWriter.println("Enter height and radius(through a space)");
-                                    String hr = bufferedReader.readLine();
-                                    printWriter.println(searchCylinderArea(hr));
-                                    logger.info("sent cylinder area");
-                                    break;
-                                case "2":
-                                    printWriter.println("Enter side");
-                                    int a = Integer.parseInt(bufferedReader.readLine());
-                                    printWriter.println(searchCubeArea(a));
-                                    logger.info("sent cube area");
-                                    break;
-                                case "3":
-                                    printWriter.println("Enter height and radius(through a space)");
-                                    String hr2 = bufferedReader.readLine();
-                                    printWriter.println(searchConeArea(hr2));
-                                    logger.info("sent cone area");
-                                    break;
-                            }
+                            sendAll(exitOrNo, out);
                         }
+
                     } catch (IOException e) {
-                        logger.info("error");
                         System.out.println(e.getMessage());
                     }
-                }
-            }));
+                }));
+            }
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
         }
     }
 
-    public static double searchCylinderArea(String hr) {
-        double p = 3.14;
-        int[] array = doSplit(hr);
-        return p * (array[1] * array[1]) * array[0];
-    }
-
-    public static int searchCubeArea(int a) {
-        return a * a * a;
-    }
-
-    public static double searchConeArea(String hr) {
-        int[] array = doSplit(hr);
-        return 0.33 * (array[1] * array[1]) * array[0];
-    }
-
-    public static int[] doSplit(String str) {
-        String[] arrayStr = str.split(" ");
-        int[] arrayInt = {Integer.parseInt(arrayStr[0]), Integer.parseInt(arrayStr[1])};
-        return arrayInt;
+    private static void sendAll(String message, PrintWriter out) {
+        concurrentHashMap.forEach((k, y) -> {
+            System.out.println("k="+k+", y="+y);
+            out.println(message);
+        });
     }
 }
